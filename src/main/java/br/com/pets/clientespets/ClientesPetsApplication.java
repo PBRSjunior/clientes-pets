6082e4467f1a8b546ee48e3f1b1c4208ce3d13ba
paulo <pbrsj1385@gmail.com>
2024-04-24 21:45:50 -0300

    inicia o projeto

---
+++ src/main/java/br/com/pets/clientespets/ClientesPetsApplication.java
@@ -0,0 +1,13 @@
+package br.com.pets.clientespets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesPetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesPetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pets/clientespets/ClientesPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pets.clientespets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesPetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
