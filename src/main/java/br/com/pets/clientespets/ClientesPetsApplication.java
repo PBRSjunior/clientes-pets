de6cee9bd4fe91f1987d66c68923fe07917ed739
paulo <pbrsj1385@gmail.com>
2024-04-29 22:25:48 -0300

    implementa api de teste

--- src/main/java/br/com/pets/clientespets/ClientesPetsApplication.java
+++ src/main/java/br/com/pets/clientespets/ClientesPetsApplication.java
@@ -2,10 +2,20 @@ package br.com.pets.clientespets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClientesPetsApplication {
 
+	@GetMapping
+	public String getHometeste( ) {
+		return "Cliente Pet - API Home";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ClientesPetsApplication.class, args);
 	}
